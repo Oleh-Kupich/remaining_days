2bceb8ff1714b1166df36a88f53d6d0b48b133de
okupich <okupich@softserveinc.com>
2023-10-23 15:15:48 +0300

    Enabled back all screen orientation for Android, adjusted bottomsheet height base on screen size

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -30,9 +30,11 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    _storage.readSelected().then((value) => setState(() {
-      if (value != null) _region = value;
-    }),);
+    _storage.readSelected().then(
+          (value) => setState(() {
+            if (value != null) _region = value;
+          }),
+        );
   }
 
   void _addStay(StayItem stayItem) {
@@ -40,8 +42,10 @@ class _HomePageState extends State<HomePage> {
 
     setState(() {
       _region.stays.add(stayItem);
-      _region.stays.sort((range1, range2) =>
-        _region.sortAscending ? range2.start.compareTo(range1.start) : range1.start.compareTo(range2.start),);
+      _region.stays.sort(
+        (range1, range2) =>
+            _region.sortAscending ? range2.start.compareTo(range1.start) : range1.start.compareTo(range2.start),
+      );
     });
     _storage.write(_region);
   }
@@ -58,17 +62,19 @@ class _HomePageState extends State<HomePage> {
     _storage.write(_region);
 
     setState(() {
-      _region.stays.sort((range1, range2) =>
-        _region.sortAscending ? range2.start.compareTo(range1.start) : range1.start.compareTo(range2.start),);
+      _region.stays.sort(
+        (range1, range2) =>
+            _region.sortAscending ? range2.start.compareTo(range1.start) : range1.start.compareTo(range2.start),
+      );
     });
   }
 
   void _updateRegionSettings(Map<String, dynamic> settings) {
     setState(() {
       _region
-          ..name = settings[Settings.regionName.name] as String
-          ..rollingPeriod = int.parse(settings[Settings.rollingPeriod.name] as String)
-          ..maxStay = int.parse(settings[Settings.maxStay.name] as String);
+        ..name = settings[Settings.regionName.name] as String
+        ..rollingPeriod = int.parse(settings[Settings.rollingPeriod.name] as String)
+        ..maxStay = int.parse(settings[Settings.maxStay.name] as String);
     });
     _storage.write(_region);
   }
@@ -82,22 +88,28 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  (int, String) _calculateRemainingDays(List<StayItem> entryExitDates,
-      {required int maxStay, required int rollingRange,}) {
+  (int, String) _calculateRemainingDays(
+    List<StayItem> entryExitDates, {
+    required int maxStay,
+    required int rollingRange,
+  }) {
     (int, DateTimeRange?) usedDays;
     if ((usedDays = daysStayIn(entryExitDates, rollingRange: rollingRange, skipFutureDates: true)).$1 > maxStay) {
       final overStayRange = StayItem(
-          start: DateTime(usedDays.$2!.end.year, usedDays.$2!.end.month, usedDays.$2!.end.day - (rollingRange - 1)),
-          end: usedDays.$2!.end,);
+        start: DateTime(usedDays.$2!.end.year, usedDays.$2!.end.month, usedDays.$2!.end.day - (rollingRange - 1)),
+        end: usedDays.$2!.end,
+      );
       return (maxStay - usedDays.$1, 'overstay'.tr(args: [overStayRange.toString()]));
     }
 
     var remainingDays = 0;
     final today = DateTime.now();
     while (remainingDays < maxStay &&
-        daysStayIn(entryExitDates + [StayItem(start: today, end: today.add(Duration(days: remainingDays)))],
-                    rollingRange: rollingRange, skipFutureDates: false,)
-                .$1 <=
+        daysStayIn(
+              entryExitDates + [StayItem(start: today, end: today.add(Duration(days: remainingDays)))],
+              rollingRange: rollingRange,
+              skipFutureDates: false,
+            ).$1 <=
             maxStay) {
       ++remainingDays;
     }
@@ -112,93 +124,103 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     final stays = _region.stays;
     return Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          scrolledUnderElevation: 0,
-          title: Text(_region.name),
-          centerTitle: true,
-          actions: [
-            IconButton(
-                onPressed: () => displayTextInputDialog(
-                    context: context,
-                    config: _region,
-                    onSave: _updateRegionSettings,
-                ),
-                icon: const Icon(Icons.settings),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        scrolledUnderElevation: 0,
+        title: Text(_region.name),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () => displayTextInputDialog(
+              context: context,
+              config: _region,
+              onSave: _updateRegionSettings,
             ),
-          ],
+            icon: const Icon(Icons.settings),
+          ),
+        ],
+      ),
+      backgroundColor: Theme.of(context).canvasColor,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: GestureDetector(
+        onVerticalDragUpdate: DefaultBottomBarController.of(context).onDrag,
+        onVerticalDragEnd: DefaultBottomBarController.of(context).onDragEnd,
+        child: StaysFABWidget(
+          hasStays: stays.isNotEmpty,
+          ascending: _region.sortAscending,
+          onPressed: () => DefaultBottomBarController.of(context).swap(),
+          onSort: _sort,
         ),
-        backgroundColor: Theme.of(context).canvasColor,
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: GestureDetector(
-            onVerticalDragUpdate: DefaultBottomBarController.of(context).onDrag,
-            onVerticalDragEnd: DefaultBottomBarController.of(context).onDragEnd,
-            child: StaysFABWidget(
-              hasStays: stays.isNotEmpty,
-              ascending: _region.sortAscending,
-              onPressed: () => DefaultBottomBarController.of(context).swap(),
-              onSort: _sort,
-            ),),
-        bottomNavigationBar: GestureDetector(
+      ),
+      bottomNavigationBar: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          final collapsedHeight = constraints.maxHeight / 4;
+          final expandedHeight = constraints.maxHeight / 3;
+          return GestureDetector(
             onTap: _showDateRangePicker,
             child: BottomExpandableAppBar(
-                appBarHeight: 200,
-                expandedHeight: 200,
-                horizontalMargin: 0,
-                shape: const AutomaticNotchedShape(RoundedRectangleBorder(), StadiumBorder(side: BorderSide())),
-                expandedBody: NotificationListener<ScrollEndNotification>(
-                  onNotification: (scrollNotification) {
-                    final bottomBarController = DefaultBottomBarController.of(context);
-                    final metrics = scrollNotification.metrics;
-                    final scrollIsAtTop = metrics.atEdge && metrics.pixels == 0;
-                    if (_scrollWasAtTop && bottomBarController.isOpen && scrollIsAtTop) {
-                      bottomBarController.close();
-                    }
-                    _scrollWasAtTop = scrollIsAtTop;
-                    return true;
-                  },
-                  child: ListView.separated(
-                    padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
-                    itemCount: stays.isEmpty ? 1 : stays.length,
-                    itemBuilder: (context, index) => stays.isEmpty
-                        ? EmptyStayListItemWidget(addStay: _showDateRangePicker)
-                        : StayListItemWidget(
-                            stayListItem: stays[index],
-                            onRemove: _removeStay,
-                            onEdit: (item) => _showDateRangePicker(editingItem: item),
-                            onAdd: _showDateRangePicker,
-                            leadingWidget: Text('${stays.length - index}'),
-                          ),
-                    separatorBuilder: (context, index) => const Divider(),
-                  ),
-                ),),),
-        body: LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints constraints) {
-            final remainingDays =
-                _calculateRemainingDays(
-                    stays,
-                    maxStay: _region.maxStay,
-                    rollingRange: _region.rollingPeriod,
-                );
-            return Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'day'.plural(remainingDays.$1),
-                        style: TextStyle(
-                            color: remainingDays.$1 < 0 ? Colors.red : null, fontSize: max(constraints.maxHeight / 10, 28),),
-                      ),
-                      Text(
-                        remainingDays.$2,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: max(constraints.maxHeight / 24, 14)),
-                      ),
-                    ],
+              appBarHeight: collapsedHeight,
+              expandedHeight: expandedHeight,
+              horizontalMargin: 0,
+              shape: const AutomaticNotchedShape(RoundedRectangleBorder(), StadiumBorder(side: BorderSide())),
+              expandedBody: NotificationListener<ScrollEndNotification>(
+                onNotification: (scrollNotification) {
+                  final bottomBarController = DefaultBottomBarController.of(context);
+                  final metrics = scrollNotification.metrics;
+                  final scrollIsAtTop = metrics.atEdge && metrics.pixels == 0;
+                  if (_scrollWasAtTop && bottomBarController.isOpen && scrollIsAtTop) {
+                    bottomBarController.close();
+                  }
+                  _scrollWasAtTop = scrollIsAtTop;
+                  return true;
+                },
+                child: ListView.separated(
+                  padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+                  itemCount: stays.isEmpty ? 1 : stays.length,
+                  itemBuilder: (context, index) => stays.isEmpty
+                      ? EmptyStayListItemWidget(addStay: _showDateRangePicker)
+                      : StayListItemWidget(
+                          stayListItem: stays[index],
+                          onRemove: _removeStay,
+                          onEdit: (item) => _showDateRangePicker(editingItem: item),
+                          onAdd: _showDateRangePicker,
+                          leadingWidget: Text('${stays.length - index}'),
+                        ),
+                  separatorBuilder: (context, index) => const Divider(),
                 ),
-            );
-          },
-        ),
+              ),
+            ),
+          );
+        },
+      ),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          final remainingDays = _calculateRemainingDays(
+            stays,
+            maxStay: _region.maxStay,
+            rollingRange: _region.rollingPeriod,
+          );
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'day'.plural(remainingDays.$1),
+                  style: TextStyle(
+                    color: remainingDays.$1 < 0 ? Colors.red : null,
+                    fontSize: max(constraints.maxHeight / 10, 28),
+                  ),
+                ),
+                Text(
+                  remainingDays.$2,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: max(constraints.maxHeight / 24, 14)),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
